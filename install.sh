8d09c44b57f35bef5435f77f32a8243156e81c3c
Batwam <56710655+Batwam@users.noreply.github.com>
2024-01-26 16:28:15 +0800

    dummy change for GPG test

--- install.sh
+++ install.sh
@@ -73,6 +73,7 @@ gnome-extensions enable $extension
 if [ "$XDG_SESSION_TYPE" = "x11" ]; then
 	printf "\n\e[32mAll files copied. \nReloading the gnome-shell (shortcut Alt + F2, r) to load the extension.\n\n\e[0m"
 	#killall -3 gnome-shell
+	
 	gnome-extensions enable $extension
 else
 	printf "\n\e[32mAll files copied. \nPlease log out and log back in again to load the extension.\n\n\e[0m"
